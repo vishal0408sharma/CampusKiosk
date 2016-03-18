b5b102fd2838ba08bdcd346559d13ceb4b2806e7
vishal0408sharma <vishal0408sharma@gmail.com>
2016-03-18 19:56:51 +0530

    Added js, css and font files

---
+++ css/f_css/default.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>KIET</title>
+</head>
+
+<body>
+    <link rel="stylesheet" type="text/css" href="../css/s_css/entry.css">
+
+    <div id="tagline">
+    	We are not telling you,<br>its going to be easy.<br>
+		We are telling you,<br>its going to be worth it.
+		
+    </div>
+        
+    <div >
+	
+    </div>
+
+</div><!--end of content -->
+</body>
+</html>
